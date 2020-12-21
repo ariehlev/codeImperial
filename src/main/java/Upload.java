a602c7901e64aaa09a482d03f0d58bca9e9b278b
Kazuyachi11 <ivan.busha18@imperial.ac.uk>
2020-12-21 20:21:46 +0300

    v1.3.5

--- src/main/java/Upload.java
+++ src/main/java/Upload.java
@@ -30,81 +30,85 @@ public class Upload implements ActionListener{
             File file_path = file_select.getSelectedFile();
             System.out.println(file_path.getAbsolutePath());
             System.out.println(FilenameUtils.getExtension(String.valueOf(file_path)));
-            JFrame new_frame = new JFrame("Enlarged Picture");
-            Image image1 = null;
-            try {
-                image1= ImageIO.read(file_path);
-            } catch (IOException ioException) {
-                ioException.printStackTrace();
-            }
-            JLabel pic = new JLabel();
-
-            JLabel nam = new JLabel("Name: ");
-            JLabel descr = new JLabel("Description: ");
-
-            JTextField name = new JTextField();
-            JTextField desc = new JTextField();
-
-            JPanel big_pic = new JPanel();
-            JPanel text = new JPanel();
-
-            JButton upload = new JButton("Upload");
-
-
-            big_pic.setSize(750,600);
-            pic.setSize(750,600);
-
-            pic.setIcon(new ImageIcon(new ImageIcon(image1).getImage().getScaledInstance(750, 600, Image.SCALE_DEFAULT)));
-            big_pic.add(pic);
-
-            GroupLayout layout = new GroupLayout(text);
-            text.setLayout(layout);
-            layout.setAutoCreateGaps(true);
-            layout.setAutoCreateContainerGaps(true);
-            layout.setHorizontalGroup(layout.createSequentialGroup()
-                    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                            .addComponent(nam)
-                            .addComponent(descr)
-                            .addComponent(upload)
-                    )
-                    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                            .addComponent(name)
-                            .addComponent(desc)
-                    )
-
-            );
-            layout.setVerticalGroup(layout.createSequentialGroup()
-                    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                            .addComponent(nam)
-                            .addComponent(name)
-                    )
-                    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                            .addComponent(descr)
-                            .addComponent(desc)
-                    )
-                    .addComponent(upload)
-            );
-            upload.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    String name_input=name.getText();
-                    String description_input=desc.getText();
-                    ArrayList<String> data_upload = new ArrayList<String>();
-                    data_upload.add(name_input);
-                    data_upload.add(description_input);
-
+            if(FilenameUtils.getExtension(String.valueOf(file_path))=="jpg"){
+                JFrame new_frame = new JFrame("Enlarged Picture");
+                Image image1 = null;
+                try {
+                    image1= ImageIO.read(file_path);
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
                 }
-            });
+                JLabel pic = new JLabel();
+
+                JLabel nam = new JLabel("Name: ");
+                JLabel descr = new JLabel("Description: ");
+
+                JTextField name = new JTextField();
+                JTextField desc = new JTextField();
+
+                JPanel big_pic = new JPanel();
+                JPanel text = new JPanel();
+
+                JButton upload = new JButton("Upload");
 
 
-            text.setBounds(750,0,400,600);
+                big_pic.setSize(750,600);
+                pic.setSize(750,600);
 
-            new_frame.add(big_pic);
-            new_frame.add(text);
-            new_frame.setSize(1200,600);
-            new_frame.setLayout(null);
-            new_frame.setVisible(true);
-            new_frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                pic.setIcon(new ImageIcon(new ImageIcon(image1).getImage().getScaledInstance(750, 600, Image.SCALE_DEFAULT)));
+                big_pic.add(pic);
+
+                GroupLayout layout = new GroupLayout(text);
+                text.setLayout(layout);
+                layout.setAutoCreateGaps(true);
+                layout.setAutoCreateContainerGaps(true);
+                layout.setHorizontalGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                .addComponent(nam)
+                                .addComponent(descr)
+                                .addComponent(upload)
+                        )
+                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                .addComponent(name)
+                                .addComponent(desc)
+                        )
+
+                );
+                layout.setVerticalGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                .addComponent(nam)
+                                .addComponent(name)
+                        )
+                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                .addComponent(descr)
+                                .addComponent(desc)
+                        )
+                        .addComponent(upload)
+                );
+                upload.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        String name_input=name.getText();
+                        String description_input=desc.getText();
+                        ArrayList<String> data_upload = new ArrayList<String>();
+                        data_upload.add(name_input);
+                        data_upload.add(description_input);
+
+                    }
+                });
+
+
+                text.setBounds(750,0,400,600);
+
+                new_frame.add(big_pic);
+                new_frame.add(text);
+                new_frame.setSize(1200,600);
+                new_frame.setLayout(null);
+                new_frame.setVisible(true);
+                new_frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+            }
+
 
         }
     }
